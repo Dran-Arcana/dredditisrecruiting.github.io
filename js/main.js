a635864814ec9a1cb30161eeb558b9c28218fc35
mtrtony <tony@morethanrewards.com>
2015-03-26 12:41:43 -0500

    Embedded video randomized from available in manifest.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+$(function(){
+    $.ajax({
+        method: "GET",
+        cache: false,
+        url: "/json/videos.json"
+    })
+    .success(function(data, textStatus, jqxhr) {
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+        };
 
+        var url = "https://www.youtube.com/embed/" + data.videoIds[getRandomInt(0,data.videoIds.length)];
+        $("#videoFrame").attr("src", url);
+        $("div.outerContainer").show();
+
+    })
+    .error(function(jqxhr, textStatus, errorThrown) {
+        console.log("You done fucked up!");
+    })
+});
